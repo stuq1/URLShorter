a48b5f5196473ebf18730026e4642f6e1566df8c
Aleksey Stupnikov <stuq11@gmail.com>
2025-01-25 09:46:30 +0500

    Инициализирующий коммит, создано окружение для бэка

---
+++ backend/Program.cs
@@ -0,0 +1,15 @@
+namespace url_shorter_backend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Test");
+
+            app.Run();
+        }
+    }
+}
