5aa4e5bb2eb037bf20c68741923058e1cbb60216
Aleksey Stupnikov <stuq11@gmail.com>
2025-01-25 21:00:57 +0500

    Добавлено подключение к базе данных и реализовано api для работы с учетными записями пользователей

---
+++ backend/models/users/User.cs
@@ -0,0 +1,9 @@
+﻿namespace url_shorter_backend.models.users;
+
+public class User
+{
+
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+
+}
---
+++ backend/models/users/UserDB.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace url_shorter_backend.models.users;
+
+[Table("Users")]
+public class UserDB : User
+{
+
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+}
